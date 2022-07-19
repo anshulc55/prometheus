fcf13de5ad13760724df4a4f011054aae47b9ceb
Anshul Chauhan <anshulc55@gmail.com>
2022-07-19 19:13:50 +0530

    Counter Labels

--- Application Instrumentation/counter_metric.py
+++ Application Instrumentation/counter_metric.py
@@ -1,12 +1,13 @@
 import http.server
 from prometheus_client import start_http_server, Counter
 
-REQUEST_COUNT = Counter("app_request_counts", "Total HTTP Request Count")
+REQUEST_COUNT = Counter("app_request_counts", "Total HTTP Request Count", ['python_custom_app','endpoint'])
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        REQUEST_COUNT.inc()
+        #REQUEST_COUNT.inc()
+        REQUEST_COUNT.labels('get_funcation', self.endpoint).inc()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
