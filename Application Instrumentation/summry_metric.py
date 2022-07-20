2fcea23b9d4696ad8c1c6871f06533a48e3d37db
Anshul Chauhan <anshulc55@gmail.com>
2022-07-20 19:49:06 +0530

    Summry metric

--- Application Instrumentation/summry_metric.py
+++ Application Instrumentation/summry_metric.py
@@ -6,16 +6,17 @@ REQUEST_LATENCY_TIME= Summary('request_latency_time','Response latency in second
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
+    @REQUEST_LATENCY_TIME.time()
     def do_GET(self):
-        startTime = time.time()
+        #startTime = time.time()
         self.send_response(200)
         time.sleep(1)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First python Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Python application.</center></h2></body></html>", "utf-8"))
         self.wfile.close
-        end_time = time.time() - startTime
-        REQUEST_LATENCY_TIME.observe(end_time) 
+        #end_time = time.time() - startTime
+        #REQUEST_LATENCY_TIME.observe(end_time) 
 
 if __name__ == "__main__":
     start_http_server(5001)
