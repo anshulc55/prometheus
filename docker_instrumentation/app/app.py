d88aa96a2a3d6c5d186001526acfeb36e0be5668
Anshul Chauhan <anshulc55@gmail.com>
2022-07-21 21:02:19 +0530

    Docker

---
+++ docker_instrumentation/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+from prometheus_clinet import Counter, generate_latest
+
+REQUEST_COUNT = Counter("app_request_counter", "Total Number of Requests made of Application")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_team():
+    REQUEST_COUNT.inc()
+    return "Hello Team, This is from Docker WSGI...."
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), 200, mimetype="plain/text")
+
+if __name__ == "__main__":
+    app.run(hostname="0.0.0.0", port=5001)
